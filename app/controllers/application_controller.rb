b5dd68b6d05e24ade6895786f8c20645cab5dbb0
maks <maks@Debian.myguest.virtualbox.org>
2023-06-18 17:51:43 +0200

    adding logo/text logging page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
-end
+    def after_sign_in_path_for(resource)
+      # Ścieżka, na którą ma być przekierowany gracz po zalogowaniu
+      boards_path
+    end
+  
+    def after_sign_out_path_for(resource)
+      # Ścieżka, na którą ma być przekierowany gracz po wylogowaniu
+      new_user_session_path
+    end
+  end
